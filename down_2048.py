815f2472966acd644a24b9e86cc17335a0438bae
wangss <genhaoai@gmail.com>
2016-12-23 16:50:09 +0800

    1、thread支持 2、获取代理ip修改为一天获取一次

--- down_2048.py
+++ down_2048.py
@@ -5,11 +5,12 @@ import os
 from bs4 import BeautifulSoup
 from Utils import get_dir_size, get_every_max, user_agents
 from down_proxy import down_load_proxy
+from threading import Thread
 
 url = 'http://www.youzi4.cc/mm/'
 count_num = 3
 request_time_out = 10
-local_dir='I://ss'
+local_dir = 'I://ss'
 
 
 def parse_child_page(url='', child_num=2, proxies={}, proxy_flag=False, try_time=0, request_time_out=10):
@@ -35,7 +36,7 @@ def parse_child_page(url='', child_num=2, proxies={}, proxy_flag=False, try_time
             if r.status_code == 200:
                 if get_dir_size(local_dir) < 1000:
                     # print(pic)
-                    with open(local_dir+'//' + pic.split('/')[-1], "wb") as p:
+                    with open(local_dir + '//' + pic.split('/')[-1], "wb") as p:
                         for chunk in r.iter_content(chunk_size=1024):
                             if chunk:
                                 p.write(chunk)
@@ -59,28 +60,47 @@ def parse_child_page(url='', child_num=2, proxies={}, proxy_flag=False, try_time
                 if r.status_code == 200:
                     if get_dir_size(local_dir) < 60000:
                         # print(pic)
-                        with open(local_dir+'//' + pic.split('/')[-1], "wb") as p:
+                        with open(local_dir + '//' + pic.split('/')[-1], "wb") as p:
                             for chunk in r.iter_content(chunk_size=1024):
                                 if chunk:
                                     p.write(chunk)
                                     p.flush()
                             p.close()
             except:
-                parse_child_page(url, child_num, proxies, False, try_time + 1,request_time_out)
+                parse_child_page(url, child_num, proxies, False, try_time + 1, request_time_out)
         else:
             print('无法下载')
 
 
-pp = down_load_proxy()
-if not os.path.exists(local_dir):  # 判断是否存在，如果不存在那么新建
-    os.mkdir(local_dir)
-for j in range(1000, 9999, 1):
-    max_num = get_every_max(url, str(j))
-    if max_num == 0:
-        continue
-    start = time.clock()
-    for i in range(1, max_num + 1):
-        proxies = {"http": pp[random.randint(0, len(pp) - 1)]}
-        parse_child_page(url=url + str(j) + '/' + str(j) + '_', child_num=i, proxies=proxies, proxy_flag=False,
-                         request_time_out=10)
-    print(time.clock() - start)
+class down_img_thread(Thread):
+    def __init__(self, url, child_num, proxies, proxy_flag, request_time_out):
+        self.url = url
+        self.child_num = child_num
+        self.proxies = proxies
+        self.proxy_flag = proxy_flag
+        self.request_time_out = request_time_out
+        super(down_img_thread, self).__init__()
+
+    def run(self):
+        parse_child_page(url=self.url, child_num=self.child_num, proxies=self.proxies, proxy_flag=self.proxy_flag,
+                         request_time_out=self.request_time_out)
+
+
+if __name__ == '__main__':
+    pp = down_load_proxy()
+    if not os.path.exists(local_dir):  # 判断是否存在，如果不存在那么新建
+        os.mkdir(local_dir)
+    for j in range(5000, 6006, 1):
+        max_num = get_every_max(url, str(j))
+        threads = []
+        if max_num == 0:
+            continue
+        start = time.clock()
+        for i in range(1, max_num + 1):
+            proxies = {"http": pp[random.randint(0, len(pp) - 1)]}
+            t = down_img_thread(url + str(j) + '/' + str(j) + '_', i, proxies, False, request_time_out)
+            threads.append(t)
+            t.start()
+        for th in threads:
+            th.join()
+        print(time.clock() - start)
--- down_proxy.py
+++ down_proxy.py
@@ -1,21 +1,34 @@
 # -*- coding: utf-8 -*-
-import requests
+import requests, datetime
 from bs4 import BeautifulSoup
 
 headers = {
-        'User-Agent': "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.1 (KHTML, like Gecko) Chrome/22.0.1207.1 Safari/537.1",
-        'Accept': "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
-        'Accept-Encoding': 'gzip',
-    }
+    'User-Agent': "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.1 (KHTML, like Gecko) Chrome/22.0.1207.1 Safari/537.1",
+    'Accept': "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
+    'Accept-Encoding': 'gzip',
+}
 
 
 def down_load_proxy():
     ll = []
+    is_retry = True
+    with open('proxy.txt', 'r') as pr:
+        for i in pr:
+            if i[:-1] == datetime.datetime.now().strftime('%x'):#每天只更新一次
+                is_retry = False
+                continue
+            ll.append(i[:-1])
+    pr.close()
+    if not is_retry:
+        return ll
+    else:
+        ll=[]
     with open('proxy.txt', 'w') as of:
+        of.write('%s\n' % (datetime.datetime.now().strftime('%x')))
         for page in range(1, 2):
             url = 'http://www.xicidaili.com/nn/%s' % page
-            doc = requests.get(url, headers=headers).text
-            soup = BeautifulSoup(doc,"html.parser")
+            doc = requests.get(url, headers=headers, timeout=5).text
+            soup = BeautifulSoup(doc, "html.parser")
             trs = soup.find('table', {"id": "ip_list"}).findAll('tr')
             # print(trs[1:][0].findAll('td')[5])
             # print(trs[1:][0].findAll('td')[1])
@@ -27,6 +40,7 @@ def down_load_proxy():
                 protocol = tds[5].text.strip()
                 if protocol == 'HTTP' and get_aval_ip(ip + ":" + port):  # or protocol == 'HTTPS'
                     of.write('%s=%s:%s\n' % (protocol, ip, port))
+                    of.flush()
                     ll.append(ip + ":" + port)
         of.close()
         return ll
@@ -34,7 +48,7 @@ def down_load_proxy():
 
 def get_aval_ip(ip):
     try:
-        c = requests.get('https://www.baidu.com/', headers=headers, proxies={'http': ip})
+        c = requests.get('https://www.baidu.com/', headers=headers, proxies={'http': ip}, timeout=5)
         if c.status_code != 200:
             return False
         else:
